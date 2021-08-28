98e06a266f7ea476be79da16ec1c7bcdfb0e0423
devShaurya <devshaurya1001@gmail.com>
2021-08-29 02:08:02 +0530

    inital auth and routing

--- main.go
+++ main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
+func authMiddleware(h http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		bearerAndToken :=  strings.Split(r.Header.Get("Authorization")," ")
+
+		if len(bearerAndToken) != 2 || bearerAndToken[0] != "Bearer" || bearerAndToken[1] != "valid-key" {
+            w.Header().Set("Content-Type","application/json")
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(map[string]interface{}{"message":"Missing auth token"})
+            return
+        }
+		log.Println("User logged successfully")
+		h.ServeHTTP(w,r)	
+    })
+}
+
+func initializeRouter() *mux.Router{
+	
+	r := mux.NewRouter()
+    r.Use(authMiddleware)
+	r.HandleFunc("/todo",func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type","application/json")
+		rw.WriteHeader(http.StatusOK)
+		json.NewEncoder(rw).Encode(map[string]interface{}{"message":"Hi"})
+	}).Methods("GET")
+
+
+	return r
+}
+
 func main(){
-	fmt.Println("Started Router")
+
+	r := initializeRouter()
+	log.Println("Starting Server at port 5000")
+	log.Fatal(http.ListenAndServe(":5000",r))
 }
