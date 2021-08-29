fb57ce2f56c40f48fffbd6fb58965074b2e73500
devShaurya <devshaurya1001@gmail.com>
2021-08-29 18:23:10 +0530

    added custom date type

---
+++ modals.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+)
+
+type Date struct{
+	sql.NullTime
+}
+func (date Date) MarshalJSON() ([]byte, error) {
+	// s := time.Parse("02-01-2006",date.Time)
+	if(date.Valid){
+		return json.Marshal(fmt.Sprintf("%02d-%02d-%d",date.Time.Day(),date.Time.Month(),date.Time.Year()))
+	}
+	return json.Marshal(nil)
+}
+  
+func (date *Date) UnmarshalJSON(data []byte) error {
+	s := strings.Trim(string(data), "\"")
+	if s!="null" {
+		t, err := time.Parse("02-01-2006",s)
+		if err != nil {
+			return err
+		}
+		date.Time = t
+		date.Valid = true
+	}else{
+		date.Time = time.Time{}
+		date.Valid = false
+	}
+	return nil
+}
+
+type TodoItemCreateRequest struct{
+	Name 			string 		`json:"name"`
+	Description		string 		`json:"description"`
+	Priority		string      `json:"priority"`
+	DueDate			Date        `json:"dueDate"`
+	Completed		bool   		`json:"completed"`
+	CompletionDate 	Date 	    `json:"completionDate"`
+}
